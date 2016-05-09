c04c2c985d526804980eaf00cbe40f8cc90b566f
QuoniamYIF <yxwang19941030@163.com>
2016-05-09 11:02:19 +0800

    奇偶排序(2)

--- two_even_odd_sort.cpp
+++ two_even_odd_sort.cpp
@@ -1,6 +1,25 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main()
-{
-    return 0;
+
+int main() {
+  int a[10], temp;
+  for(int i = 0;i < 10;i ++){
+      cin >> a[i];
+  }
+  for(int i = 0;i < 9;i ++){
+      for(int j = 1;j < 10 - i;j ++){
+          int leftisEven = a[j-1] % 2 == 0;
+          int rightisEven = a[j] % 2 == 0;
+          if((leftisEven && !rightisEven) ||
+             ((leftisEven == rightisEven) && a[j-1] > a[j])){
+             temp = a[j-1];
+             a[j-1] = a[j];
+             a[j] = temp;
+          }
+      }
+  }
+  for(int i = 0;i < 10;i ++){
+      cout << a[i] << " ";
+  }
+  return 0;
 }
