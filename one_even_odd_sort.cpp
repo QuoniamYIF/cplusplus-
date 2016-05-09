4ad46ca549b452cfd998d2ab756e59245adb5988
QuoniamYIF <yxwang19941030@163.com>
2016-05-09 10:05:57 +0800

    奇偶排序(1)

---
+++ one_even_odd_sort.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a[10],n;
+    int l=0, r=9;
+    int temp;
+    for(int i = 0;i < 10;i ++){
+        cin >> a[i];
+    }
+    while(l <= r){
+        int leftisOdd = a[l] % 2 == 1;
+        int rightisEven = a[r] % 2 == 0;
+        if(leftisOdd){
+            l++;
+        }
+        if(rightisEven){
+            r--;
+        }
+        if(!leftisOdd && !rightisEven){
+            temp = a[l];
+            a[l] = a[r];
+            a[r] = temp;   
+            l++; r--;              
+        }
+    }
+    int start = 0, end = l;
+    for(int i = start;i < end;i ++){
+        for(int j = start+1;j < end;j ++){
+            if(a[i] > a[j]){
+                temp = a[j];
+                a[j] = a[i];
+                a[i] = temp;
+            }
+        }
+    }
+    start = l; end = 10;
+    for(int i = start;i < end;i ++){
+        for(int j = start+1;j < end;j ++){
+            if(a[i] > a[j]){
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }    
+    }
+    for(int i = 0;i < 10;i ++){
+        cout << a[i] << endl;
+    }
+    return 0;
+}
+  
