161e9a41ea02dd7fa4914feb100b653337dd1837
QuoniamYIF <yxwang19941030@163.com>
2016-05-10 15:35:25 +0800

    选择排序

---
+++ choosesort.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n, temp;
+    int a[1000];
+    cin >> n;
+    for(int i = 0;i < n;i ++){
+        cin >> a[i];
+    }
+    for(int j = 0;j < n;j ++){
+        for(int i = j+1; i < n;i ++){
+            if(a[j] > a[i]){
+                temp = a[j];
+                a[j] = a[i];
+                a[i] = temp;
+            }
+        }
+    }
+    for(int i = 0;i < n;i ++){
+        cout << a[i] << endl;
+    }
+}
