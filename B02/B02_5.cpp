f30b272d84f66c4f7fe78f88efb57318838ad849
QuoniamYIF <yxwang19941030@163.com>
2016-05-11 15:29:53 +0800

    http://pkuic.openjudge.cn/hw02/5/

---
+++ B02/B02_5.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int m, a[3];
+    int iC = 0;
+    cin >> m;
+    while(m > 0){
+        a[iC++]=m%10;
+        m /= 10;
+    }
+    while(iC > 0){
+        cout << a[--iC] <<endl;
+    }
+    return 0;
+}
