8d0ef2ff715459f90d3b5252c561441df93960bf
QuoniamYIF <yxwang19941030@163.com>
2016-05-11 15:07:17 +0800

    http://pkuic.openjudge.cn/hw02/3/

---
+++ B02/B02_3.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n, a[100];
+    cin >> n;
+    int max = 0;
+    for(int i = 0;i < n;i ++){
+        cin >> a[i];
+    }
+    for(int i = 0;i < n;i ++){
+        max = a[i] > max ? a[i] : max;
+    }
+    cout << max << endl;
+    return 0;
+}
