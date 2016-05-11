ce3adee4b7cd45381dfe00afa09407299f90c69d
QuoniamYIF <yxwang19941030@163.com>
2016-05-11 15:22:19 +0800

    http://pkuic.openjudge.cn/hw02/4/

---
+++ B02/B02_4.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int even[6], odd[6], c[6];
+    int oddMax = 0;
+    int evenMin = 1000;
+    int evenC = 0;
+    int oddC = 0;    
+    for(int i = 0;i < 6;i ++){
+        cin >> c[i];        
+    }
+    for(int i = 0;i < 6;i ++){
+        if(c[i] % 2 == 0){
+            even[evenC++] = c[i];            
+        }
+    }
+    for(int i = 0;i < 6;i ++){
+        if(c[i] % 2 != 0){
+            odd[oddC++] = c[i];            
+        }
+    }
+    for(int i = 0;i < evenC;i ++){
+        evenMin = even[i] < evenMin ? even[i] : evenMin;
+    }
+    for(int i = 0;i < oddC;i ++){
+        oddMax = odd[i] > oddMax ? odd[i] : oddMax;
+    }
+    if(evenMin > oddMax){
+        cout << evenMin - oddMax;
+    }else{
+        cout << -(evenMin - oddMax);        
+    }
+    return 0;
+}
